893689896e685258ff48eee404dc1721bc02d732
Antonio Saldivar <jediknight_01@hotmail.com>
2022-08-22 14:05:54 -0500

    Edited placeholder

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -11,9 +11,9 @@ export const ContactForm = ({
 }) => {
   return (
     <form onSubmit={handleSubmit}>
-      <input type='text' name='name' value={name} onChange={({target}) => {setName(target.value)}} placeholder='Add Name' />
-      <input type='tel' name='phone' value={phone} onChange={({target}) => {setPhone(target.value)}} placeholder='Add Phone' pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" />
-      <input type='email' name='email' value={email} onChange={({target}) => {setEmail(target.value)}} placeholder='Add Email' />
+      <input type='text' name='name' value={name} onChange={({target}) => setName(target.value)} placeholder='Contact Name' />
+      <input type='tel' name='phone' value={phone} onChange={({target}) => setPhone(target.value)} placeholder='Contact Phone' pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" />
+      <input type='email' name='email' value={email} onChange={({target}) => setEmail(target.value)} placeholder='Contact Email' />
       <input type='submit' value='Add Contact' />
     </form>
   );
