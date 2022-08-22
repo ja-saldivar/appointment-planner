fae7a126722112fe40837b1431768a8957c3887d
Antonio Saldivar <jediknight_01@hotmail.com>
2022-08-22 14:01:07 -0500

    Added return for better syntax and corrected className

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -4,9 +4,11 @@ export const Tile = ({ tile }) => {
   return (
     <div className="tile-container">
       {Object.values(tile).map((value, index) => {
-        <p key={index} className={index === 0 ? 'tile-title' : 'tile'} >
-          {value}
-        </p>
+        return (
+          <p key={index} className={`${index === 0 ? 'tile-title ' : ''}tile`} >
+            {value}
+          </p>
+        )
       })}
     </div>
   );
